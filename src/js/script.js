7a7266dd4ba17dcd39a0b32cb2244fe3c00e2e7c
Excelsio <vapeexcelsio@gmail.com>
2024-07-10 22:06:22 +0300

    add section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,27 @@ document.querySelector('.prev').onclick = function () {
 document.querySelector('.next').onclick = function () {
   slider.goTo('next');
 };
+
+(function($) {
+$(function() {
+  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+    
+  function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.card').eq(i).toggleClass('card_active');
+            })
+        });
+    };
+
+    toggleSlide('.card__link');
+    toggleSlide('.card__link_back');
+// });
+});
+})(jQuery);
